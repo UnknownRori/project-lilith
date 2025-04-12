d19c0d22d2f985567c71d8c25ef0062a78680cc6
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2025-04-13 03:20:55 +0700

    implement scroll for mobile

--- src/composable/useVirtualScroll.ts
+++ src/composable/useVirtualScroll.ts
@@ -1,6 +1,9 @@
 import { onMounted, onUnmounted } from "vue";
 
 export function useVirtualScroll(handler: (deltaY: number) => void) {
+  let lastTouchY = 0;
+  let isTouching = false;
+
   const onWheel = (event: WheelEvent) => {
     event.preventDefault()
     handler(event.deltaY)
@@ -8,6 +11,23 @@ export function useVirtualScroll(handler: (deltaY: number) => void) {
 
   onMounted(() => {
     window.addEventListener('wheel', onWheel, { passive: false })
+    window.addEventListener('touchstart', (e) => {
+      if (e.touches.length === 1) {
+        lastTouchY = e.touches[0].clientY;
+        isTouching = true;
+      }
+    });
+    window.addEventListener('touchmove', (e) => {
+      if (isTouching && e.touches.length === 1) {
+        const current = e.touches[0].clientY;
+        const delta = lastTouchY - current;
+        lastTouchY = current;
+        handler(delta);
+      }
+    });
+    window.addEventListener('touchend', (e) => {
+      isTouching = false;
+    });
   })
 
   onUnmounted(() => {
