e7711c27063e2f8b8129786c0d17386f3e9425a4
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2025-04-13 09:30:52 +0700

    📦 added pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -14,6 +15,7 @@ export default defineConfig(({ command }) => {
       vue(),
       vueDevTools(),
       tailwindcss(),
+      VitePWA({ registerType: 'autoUpdate' }),
     ],
     resolve: {
       alias: {
