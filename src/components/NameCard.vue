56c8d3bceea0174fa55fe1d7e2ceba75e2338a62
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2025-05-30 15:21:29 +0700

    added essential social media link

--- src/components/NameCard.vue
+++ src/components/NameCard.vue
@@ -1,10 +1,12 @@
 <script setup lang='ts'>
 import { CardAngled } from '@/components/ui/angled';
+
+import { Github, Youtube, Instagram, Linkedin } from 'lucide-vue-next';
 </script>
 
 <template>
   <CardAngled class="w-full after:bg-gray-900/50 border-white after:border-2 after:-skew-y-4">
-    <div class="flex flex-col gap-2 w-full h-full p-6 text-white font-bold
+    <div class="flex flex-col gap-1 w-full h-full p-6 text-white font-bold
             text-center">
       <span class="text-2xl">
         Akbar Hendra Jaya
@@ -15,6 +17,24 @@ import { CardAngled } from '@/components/ui/angled';
       <span class="italic text-sm">
         Web Dev, Game Dev, Pixel Artist, VGM Composer
       </span>
+      <div class="flex justify-center items-center gap-2 mt-2">
+        <a href="https://github.com/UnknownRori" target="_blank" class="text-gray-400
+          hover:text-white duration-500">
+          <Github class='w-5 h-5' />
+        </a>
+        <a href="https://www.youtube.com/@unknownrori" target="_blank" class="text-gray-400
+          hover:text-white duration-500">
+          <Youtube class='w-5 h-5' />
+        </a>
+        <a href="https://www.instagram.com/unknownrori/" target="_blank" class="text-gray-400
+          hover:text-white duration-500">
+          <Instagram class='w-5 h-5' />
+        </a>
+        <a href="https://www.linkedin.com/in/unknownrori" target="_blank" class="text-gray-400
+          hover:text-white duration-500">
+          <Linkedin class='w-5 h-5' />
+        </a>
+      </div>
     </div>
   </CardAngled>
 </template>
